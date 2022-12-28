bbed1111341812aaabd178c5b96eb7ced292e013
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-12-28 12:33:34 +0530

    Added new problem

---
+++ Maths/Problems/5.cpp
@@ -0,0 +1,34 @@
+// Count number of triangles whose side is A, which is not hypotenuse
+// and triangle is right angled triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int N){
+    int ans = 0;
+    long long lim = (long long ) N*N;
+    for(int x=1;x*x<=lim;x++){
+        if(N%x==0){
+            // x * y = N
+            int y = lim/x;
+            if(x < y && (x%2 == y%2)){
+                int C = (x+y)/2;
+                int B = (y-x)/2;
+               // N * N + B*B = C*C
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        cout<<count(N)<<endl;
+    }
+}
