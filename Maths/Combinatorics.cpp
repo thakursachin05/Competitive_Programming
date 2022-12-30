355d62bef7a2917500da0f6232e839ed4e585381
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-12-30 15:27:28 +0530

    Added code for combinatorics

---
+++ Maths/Combinatorics.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll n;
+    cin>>n;
+    ll  diagonals = (n*(n-3))/2;
+    cout<<diagonals<<endl;
+    return 0;
+}
